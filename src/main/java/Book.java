198bdd987fe08825d88b8d824d02ac8088f8e0b4
lalitsaini859@gmail.com <lalitsaini859@gmail.com>
2022-08-16 12:14:29 +0530

    commit

---
+++ src/main/java/Book.java
@@ -0,0 +1,40 @@
+public class Book {
+    int title;
+    int yearOfPublication;
+    String ISBN;
+
+    public int getTitle() {
+        return title;
+    }
+
+    public void setTitle(int title) {
+        this.title = title;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(int authorName) {
+        this.authorName = authorName;
+    }
+
+    int authorName;
+
+}
---
+++ src/main/java/BookImpl.java
@@ -0,0 +1,17 @@
+public class BookImpl {
+    int title;
+    int yearOfPublication;
+    String ISBN;
+
+    public void setTitle(int title) {
+        this.title = title;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+}
